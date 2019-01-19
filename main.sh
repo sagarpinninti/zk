4c0f5fc9558a59d9e4237fea0c3fbe7f17d6d8dc
i079639 <sagar.pinninti@gmail.com>
2019-01-19 10:48:52 +0530

    c1

---
+++ main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget http://mirrors.estointernet.in/apache/zookeeper/zookeeper-3.4.13/zookeeper-3.4.13.tar.gz .;
+chmod +x /zookeeper-3.4.13.tar.gz;
+tar -xvzf /zookeeper-3.4.13.tar.gz;
